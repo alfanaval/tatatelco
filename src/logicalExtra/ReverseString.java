e66cda60173f87ab310f5aa8bd3d5698ae2b6b1f
alfanaval <118259348+alfanaval@users.noreply.github.com>
2022-11-24 15:54:45 +0530

    Update ReverseString.java

--- src/logicalExtra/ReverseString.java
+++ src/logicalExtra/ReverseString.java
@@ -4,7 +4,7 @@ public class ReverseString {
 public static void main(String[] args) {
 	/*StringBuilder s=new StringBuilder("YOGESH");
 	System.out.println(s.reverse());*/
-String s="PRATHMESH";
+String s="PRATHM";
 String rev=" ";
 for (int i=s.length()-1;i>=0;i--){
 	//rev=s.charAt(i)+rev;
